f7172e849b81828e4776bd4daf06ca72f7022c22
deboranis <dc.nisen@hotmail.com>
2020-06-03 09:51:59 -0300

    segunda

---
+++ revisao/objetos.js
@@ -0,0 +1,18 @@
+const pessoa = {
+    personalidade: ['personalidade1', 'personalidade2'],
+    areaAtuacao: 'bióloga'
+}
+
+
+const clientes = {
+    cliente1 = {
+        nome: "Dora Barreto",
+        cpf: '2222222209',
+        valorCompra: 2000,
+        modeloCelular: "XXX-12",
+        anoCelular: "2019",
+        corCelular: "amarelo",
+        loja: "SP-BR",
+        atendente: "joana maria"
+    }
+}
